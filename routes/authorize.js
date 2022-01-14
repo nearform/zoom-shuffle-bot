cdde928e9cfb40a0d6a8f372de00d6015b9f9250
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-01-14 20:41:30 +0100

    chore: fix an old env var reference

--- routes/authorize.js
+++ routes/authorize.js
@@ -2,7 +2,7 @@ export default async function (fastify) {
   fastify.get('/authorize', async (req, res) => {
     try {
       res.redirect(
-        `https://zoom.us/launch/chat?jid=robot_${process.env.ROBOT_JID}`
+        `https://zoom.us/launch/chat?jid=robot_${process.env.BOT_JID}`
       )
     } catch (error) {
       fastify.log.error(error)
