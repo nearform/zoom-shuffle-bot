99eac6855bee83d042e0283d688ec1f1e94be328
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-01-19 12:52:30 +0100

    chore: update the command

--- routes/bot.js
+++ routes/bot.js
@@ -18,7 +18,7 @@ export default async function (fastify) {
           })
         }
 
-        if (command === 'zoomtalkingstick') {
+        if (command === 'list') {
           const meeting = await getHostActiveMeeting(fastify.pg, userId)
 
           if (meeting && meeting.participants.length > 0) {
