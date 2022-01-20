c5369b395dc21f408117dcea2565dbfc4c398a43
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-01-20 13:31:03 +0100

    refactor: use a constant initialization vector for usernames encryption

--- helpers/crypto.js
+++ helpers/crypto.js
@@ -1,23 +1,21 @@
-import { createCipheriv, createDecipheriv, randomBytes } from 'crypto'
+import { createCipheriv, createDecipheriv } from 'crypto'
 
 const algorithm = 'aes256'
 const key = `${process.env.CLIENT_ID}.${process.env.CLIENT_SECRET}`
   .substr(0, 32)
   .padEnd(32)
 
+// a constant IV is fine for this usecase, since we basically need a reversible hash
+const iv = key.substr(0, 16).padEnd(16)
+
 export function encrypt(value) {
-  const iv = randomBytes(16)
   const cipher = createCipheriv(algorithm, key, iv)
 
-  const encrypted = cipher.update(value, 'utf8', 'hex') + cipher.final('hex')
-
-  return `${iv.toString('hex')}:${encrypted}`
+  return cipher.update(value, 'utf8', 'hex') + cipher.final('hex')
 }
 
 export function decrypt(value) {
-  const [iv, data] = value.split(':')
+  const decipher = createDecipheriv(algorithm, key, iv)
 
-  const decipher = createDecipheriv(algorithm, key, Buffer.from(iv, 'hex'))
-
-  return decipher.update(data, 'hex', 'utf8') + decipher.final('utf8')
+  return decipher.update(value, 'hex', 'utf8') + decipher.final('utf8')
 }
