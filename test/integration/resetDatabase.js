195933ecf43e12cc1827a3849face5746caced33
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-01-24 22:39:34 +0100

    fix: await the database reset script in integration tests

--- test/integration/resetDatabase.js
+++ test/integration/resetDatabase.js
@@ -1,4 +1,7 @@
-import { exec } from 'child_process'
+import { promisify } from 'util'
+import { exec as execCallback } from 'child_process'
+
+const exec = promisify(execCallback)
 
 export default function resetDatabase() {
   return exec('npm run db:reset')
