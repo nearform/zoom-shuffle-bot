cca726e43093c67d7f622c9fa6f2080cc6362de2
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-01-25 13:24:57 +0100

    refactor: split up to zoom plugin to facilitate mocking

--- plugins/zoom.js
+++ plugins/zoom.js
@@ -1,51 +1,19 @@
 import fp from 'fastify-plugin'
-import fetch from 'node-fetch'
-import createError from 'http-errors'
 
-import {
-  getApiTokenData,
-  getBotTokenData,
-  upsertApiTokenData,
-  upsertBotTokenData,
-} from '../services/db.js'
-import {
-  fetchTokenByCode,
-  fetchTokenByCredentials,
-  fetchTokenByRefresh,
-} from './oauth.js'
+import { getApiTokenData, upsertApiTokenData } from '../services/db.js'
+import { fetchTokenByCode, fetchTokenByRefresh } from './oauth.js'
 import validateOptions from './helpers/validateOptions.js'
 import isTokenExpired from './helpers/isTokenExpired.js'
 import getTokenExpiresOn from './helpers/getTokenExpiresOn.js'
 import verifyRequest from './verifyRequest.js'
-import { ZOOM_API_BASE_URL } from '../const.js'
+import sendBotMessage from './sendBotMessage.js'
+import apiFetch from './apiFetch.js'
 
 async function plugin(fastify, options = {}) {
   validateOptions(
     ['clientId', 'clientSecret', 'botJid', 'verificationToken', 'redirectUri'],
     options
   )
-
-  async function getBotToken(accountId) {
-    const tokenData = await getBotTokenData(fastify.pg, accountId)
-
-    if (tokenData && !isTokenExpired(tokenData)) {
-      return tokenData.access_token
-    } else {
-      const { access_token, expires_in } = await fetchTokenByCredentials(
-        options.clientId,
-        options.clientSecret
-      )
-
-      await upsertBotTokenData(
-        fastify.pg,
-        accountId,
-        access_token,
-        getTokenExpiresOn(expires_in)
-      )
-
-      return access_token
-    }
-  }
 
   async function getApiToken(accountId) {
     const tokenData = await getApiTokenData(fastify.pg, accountId)
@@ -72,24 +40,6 @@ async function plugin(fastify, options = {}) {
     }
   }
 
-  async function sendBotMessage({ toJid, accountId, content, isMarkdown }) {
-    const token = await getBotToken(accountId)
-
-    return apiFetch(token, '/im/chat/messages', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        robot_jid: options.botJid,
-        to_jid: toJid,
-        account_id: accountId,
-        content,
-        is_markdown_support: isMarkdown,
-      }),
-    })
-  }
-
   async function authorize(code) {
     const { access_token, refresh_token, expires_in } = await fetchTokenByCode(
       options.clientId,
@@ -109,28 +59,8 @@ async function plugin(fastify, options = {}) {
     )
   }
 
-  async function apiFetch(token, endpoint, fetchOptions = {}) {
-    const { headers = {} } = fetchOptions
-
-    const response = await fetch(`${ZOOM_API_BASE_URL}${endpoint}`, {
-      ...fetchOptions,
-      headers: {
-        Authorization: `Bearer ${token}`,
-        ...headers,
-      },
-    })
-
-    if (response.ok) {
-      return response.json()
-    } else {
-      const { message } = await response.json()
-
-      throw createError(response.status, message)
-    }
-  }
-
   fastify.decorate('zoom', {
-    sendBotMessage,
+    sendBotMessage: sendBotMessage.bind(null, fastify, options),
     verifyRequest: verifyRequest.bind(null, options),
     authorize,
     fetch: async (accountId, endpoint) => {
