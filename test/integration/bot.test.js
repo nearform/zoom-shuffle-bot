ecf2d376b571b055860c41e2c38bddf814989194
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-01-25 13:25:47 +0100

    chore: add integration tests for the /bot route

---
+++ test/integration/bot.test.js
@@ -0,0 +1,118 @@
+import getTestServer from './getTestServer.js'
+import resetDatabase from './resetDatabase.js'
+
+import sendBotMessage from '../../plugins/sendBotMessage.js'
+import apiFetch from '../../plugins/apiFetch.js'
+
+jest.mock('../../plugins/sendBotMessage.js')
+jest.mock('../../plugins/apiFetch.js')
+
+expect.extend({
+  botMessageContaining(received, value) {
+    return { pass: JSON.stringify(received).includes(value) }
+  },
+})
+
+describe('/bot route verification', () => {
+  it('responds with 401 when not verified', async () => {
+    const server = getTestServer()
+
+    const response = await server.inject({ url: '/bot', method: 'POST' })
+
+    expect(response.statusCode).toBe(401)
+  })
+
+  it('responds with 200 when verified', async () => {
+    const server = getTestServer()
+
+    const response = await server.inject({
+      url: '/bot',
+      method: 'POST',
+      headers: {
+        Authorization: process.env.VERIFICATION_TOKEN,
+        clientid: process.env.CLIENT_ID,
+      },
+      payload: {
+        payload: {},
+      },
+    })
+
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+describe('/bot route logic', () => {
+  beforeEach(async () => {
+    await resetDatabase()
+  })
+
+  it('sends a bot message when user has no active meetings', async () => {
+    const server = getTestServer()
+
+    expect(sendBotMessage).not.toHaveBeenCalled()
+
+    const response = await server.inject({
+      url: '/bot',
+      method: 'POST',
+      headers: {
+        Authorization: process.env.VERIFICATION_TOKEN,
+        clientid: process.env.CLIENT_ID,
+      },
+      payload: {
+        payload: {
+          userId: 'non-existing-id',
+        },
+      },
+    })
+
+    expect(response.statusCode).toBe(200)
+
+    expect(sendBotMessage).toHaveBeenCalledTimes(1)
+    expect(sendBotMessage).toHaveBeenLastCalledWith(
+      expect.anything(),
+      expect.anything(),
+      expect.botMessageContaining("Sorry, you don't seem")
+    )
+  })
+
+  it('sends a bot message with meeting topic and decoded user names when user has an active meeting', async () => {
+    const server = getTestServer()
+
+    expect(sendBotMessage).not.toHaveBeenCalled()
+    expect(apiFetch).not.toHaveBeenCalled()
+
+    apiFetch.mockResolvedValueOnce({ topic: 'Test meeting topic' })
+
+    const response = await server.inject({
+      url: '/bot',
+      method: 'POST',
+      headers: {
+        Authorization: process.env.VERIFICATION_TOKEN,
+        clientid: process.env.CLIENT_ID,
+      },
+      payload: {
+        payload: {
+          accountId: 'test_account_id',
+          userId: 'test_user_id',
+        },
+      },
+    })
+
+    expect(response.statusCode).toBe(200)
+
+    expect(apiFetch).toHaveBeenCalledTimes(1)
+    expect(sendBotMessage).toHaveBeenCalledTimes(2)
+    expect(sendBotMessage).toHaveBeenNthCalledWith(
+      1,
+      expect.anything(),
+      expect.anything(),
+      expect.botMessageContaining('Test meeting topic')
+    )
+    expect(sendBotMessage).toHaveBeenNthCalledWith(
+      2,
+      expect.anything(),
+      expect.anything(),
+      expect.botMessageContaining('test_user')
+    )
+  })
+})
