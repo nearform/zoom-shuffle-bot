864b35091936d68b979b4358380ebe689f9ae1e6
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-01-28 15:26:04 +0100

    chore: log app config

--- chatbot.js
+++ chatbot.js
@@ -13,6 +13,16 @@ const server = buildServer({
   redirectUri: process.env.REDIRECT_URL,
 })
 
+console.log('env', {
+  logger: true,
+  databaseUrl: process.env.DATABASE_URL,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  botJid: process.env.BOT_JID,
+  verificationToken: process.env.VERIFICATION_TOKEN,
+  redirectUri: process.env.REDIRECT_URL,
+})
+
 const start = async () => {
   try {
     await server.listen(process.env.PORT, '0.0.0.0')
