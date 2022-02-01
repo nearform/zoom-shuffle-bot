92b7375642f5e5e28e117b1f88a34fe12ad76937
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-02-01 15:32:08 +0100

    chore: simplify the db population config

--- postgrator-populate.cjs
+++ postgrator-populate.cjs
@@ -1,12 +1,6 @@
-require('dotenv').config()
+const defaultConfig = require('./postgrator.cjs')
 
 module.exports = {
-  migrationDirectory: 'db/population',
-  driver: 'pg',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: false,
+  ...defaultConfig,
+  migrationDirectory: 'db/population'
 }
