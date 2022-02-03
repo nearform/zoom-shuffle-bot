29b92f5d9d163da2427e854bebe03e3b1db88c89
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-02-03 19:46:56 +0100

    refactor: use env-schema for validating environment variables

--- test/integration/getTestServer.js
+++ test/integration/getTestServer.js
@@ -2,16 +2,17 @@ import buildServer from '../../server.js'
 
 export default function getTestServer() {
   return buildServer({
-    log: false,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    botJid: process.env.BOT_JID,
-    verificationToken: process.env.VERIFICATION_TOKEN,
-    redirectUri: process.env.REDIRECT_URL,
-    dbUser: process.env.DB_USER,
-    dbPassword: process.env.DB_PASSWORD,
-    dbPort: process.env.DB_PORT,
-    dbHost: process.env.DB_HOST,
-    dbName: process.env.DB_NAME,
+    LOG_LEVEL: 'silent',
+    PRETTY_PRINT: false,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    BOT_JID: process.env.BOT_JID,
+    VERIFICATION_TOKEN: process.env.VERIFICATION_TOKEN,
+    REDIRECT_URL: process.env.REDIRECT_URL,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
   })
 }
