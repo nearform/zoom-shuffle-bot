2ec8b513aa193ec95e1f2527d68e5a285664a32d
Mauro Murru <murru7@gmail.com>
2022-03-08 20:46:07 +0100

    move envvar to envfile (#76)

--- .jest/env.js
+++ .jest/env.js
@@ -1,9 +1,3 @@
 import dotenv from 'dotenv'
 
-dotenv.config()
-
-process.env.CLIENT_ID = 'client_id'
-process.env.CLIENT_SECRET = 'client_secret'
-process.env.BOT_JID = 'zoom_bot_jid'
-process.env.VERIFICATION_TOKEN = 'zoom_verification_token'
-process.env.REDIRECT_URL = 'zoom_redirect_url'
+dotenv.config({ path: '.env.test' })
