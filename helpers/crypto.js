4795b85f06a09e2d38c73f243ab8be2b1303e319
Mauro Murru <murru7@gmail.com>
2022-03-11 11:08:56 +0100

    remove comment and replace obsolete substr (#80)

--- helpers/crypto.js
+++ helpers/crypto.js
@@ -2,11 +2,11 @@ import { createCipheriv, createDecipheriv } from 'crypto'
 
 const algorithm = 'aes256'
 const key = `${process.env.CLIENT_ID}.${process.env.CLIENT_SECRET}`
-  .substr(0, 32)
+  .substring(0, 32)
   .padEnd(32)
 
 // a constant IV is fine for this usecase, since we basically need a reversible hash
-const iv = key.substr(0, 16).padEnd(16)
+const iv = key.substring(0, 16).padEnd(16)
 
 export function encrypt(value) {
   const cipher = createCipheriv(algorithm, key, iv)
